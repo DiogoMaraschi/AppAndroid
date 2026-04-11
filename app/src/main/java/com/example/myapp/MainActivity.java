269c546f13cf941464130b629907c5b1ddba3ccf
DiogoMaraschi <maraschidiogo@gmail.com>
2026-04-11 15:57:21 -0300

    tratamento de erro

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.myapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     TextView textView;
 
-    EditText numMin;
-    EditText numMax;
+    EditText edMin;
+    EditText edMax;
 
 
     int i;
@@ -33,8 +35,8 @@ public class MainActivity extends AppCompatActivity {
 
         button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
-        numMin = findViewById(R.id.edMin);
-        numMax = findViewById(R.id.edMax);
+        edMin = findViewById(R.id.edMin);
+        edMax = findViewById(R.id.edMax);
 
         EdgeToEdge.enable(this);
 
@@ -48,13 +50,67 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Random r = new Random();
-                int min = Integer.parseInt(numMin.getText().toString());
-                int max = Integer.parseInt(numMax.getText().toString());
-               // i = r.nextInt(min,max);
+
+                String strMin = edMin.getText().toString();
+                String strMax = edMax.getText().toString();
+
+                if (strMin.isEmpty()){
+                    edMin.setError("Informe um número");
+                    edMin.requestFocus();
+                    return;
+                }
+
+                if (strMax.isEmpty()){
+                    edMax.setError("Informe um número");
+                    edMin.requestFocus();
+                    return;
+                }
+
+                int min = Integer.parseInt(strMin);
+                int max = Integer.parseInt(strMax);
+
+
+
                 i = r.nextInt(max - min) + min;
                 button.setText(Integer.toString(i));
-
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v("cicloVida", "OnStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v("cicloVida", "OnRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("cicloVida", "OnDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v("cicloVida", "OnPause");
+        Toast.makeText(this, "onPause",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v("cicloVida", "OnStop");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v("cicloVida", "OnResume");
     }
 }
